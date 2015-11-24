94822051c1e483bfa09657989703f8f9001a0e66
Marco Balmer <marco@balmer.name>
2015-11-24 18:27:39 +0100

    Optimize checkMotion.sh

--- Raspberry-Pi/checkMotion.sh
+++ Raspberry-Pi/checkMotion.sh
@@ -8,9 +8,6 @@ led_default() {
 default_trigger_led0="none [mmc0] timer oneshot heartbeat backlight gpio cpu0 default-on input rfkill0 phy0rx phy0tx phy0assoc phy0radio"
 default_trigger_led1="none mmc0 timer oneshot heartbeat backlight gpio cpu0 default-on [input] rfkill0 phy0rx phy0tx phy0assoc phy0radio"
 
-echo "$default_trigger_led0" >/sys/class/leds/led0/trigger
-#echo "$default_trigger_led1" >/sys/class/leds/led1/trigger
-echo 0 >/sys/class/leds/led1/trigger
 } # led_default
 
 disable_led_triggers() {
@@ -36,7 +33,7 @@ led_red() {
 
 if [ -z `pidof motion` ];
 then
-  led_default
+  led_green
 else
   if curl -sf http://localhost:8080/0/detection/status | grep PAUSE >/dev/null;
   then
